89deef4d7e492b250aa2521468bddf1a2e7f0253
Arkadiusz Chmura <arekchmura@gmail.com>
2021-04-16 10:59:31 +0200

    Task 5

---
+++ MDBLab/MDBLab-ejb/src/java/ejb/NewsMDB.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSDestinationDefinition;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+import javax.jms.TextMessage;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Arkadiusz
+ */
+@JMSDestinationDefinition(name = "java:app/jms/NewsQueue", interfaceName = "javax.jms.Queue", resourceAdapter = "jmsra", destinationName = "NewsQueue")
+@MessageDriven(activationConfig = {
+    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "java:app/jms/NewsQueue"),
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+})
+public class NewsMDB implements MessageListener {
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    public NewsMDB() {
+    }
+    
+    @Override
+    public void onMessage(Message message) {
+        TextMessage msg = null;
+        try {
+            if (message instanceof TextMessage) {
+                msg = (TextMessage) message;
+                
+                String[] components = msg.getText().split("\\|");
+                NewsItem newsItem = new NewsItem();
+                newsItem.setHeading(components[0]);
+                newsItem.setBody(components[1]);
+                
+                em.persist(newsItem);
+            }
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
